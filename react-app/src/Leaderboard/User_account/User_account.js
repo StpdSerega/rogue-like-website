7708529849a7689935a3bfecd897b76d0559d104
Maksym45 <maks.meleshko12@gmail.com>
2023-11-12 18:20:13 +0200

    made the right design

--- react-app/src/Leaderboard/User_account/User_account.js
+++ react-app/src/Leaderboard/User_account/User_account.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./User_account.css";
 import DefaultUser from './default-user.png';
+import ArrrowBack from './back.png';
+import EditPencil from './pencil.png';
+import { Link } from "react-router-dom";
 
 let name_user = "User"
 let surname = "Super"
@@ -11,15 +14,24 @@ let score = 1234
 export default function Account() {
   return(
     <div className='Account'>
-        <h1 className='Hello_user'>Hello, {name_user}</h1>
+        <h1 className='Hello_user'>Hello, {name_user}!</h1>
         <div className='User_data'>
+            <button type="back" className="User_data_back">
+              <Link to="/">
+                  <img src={ArrrowBack} alt="back" className="User_data_back" />
+              </Link>
+              </button>
             <img src={DefaultUser} alt="DefUser" className="User_data_DefaultUser" />
-            <span className='User_data_Name'>Name: {name_user}</span>
-            <span className='User_data_Surname'>Surname: {surname}</span>
-            <span className='User_data_Nickname'>Nickname: {nickname}</span>
-
-            <span className='User_data_Age'>Age: {age}</span>
-            <span className='User_data_Score'>your best score: {score}</span>
+            <span className='User_data_text'>Name: {name_user}</span>
+            <span className='User_data_text'>Surname: {surname}</span>
+            <div className='Nick'>
+              <span className='User_data_Nick_text'>Nickname: {nickname}</span>
+              <button type="back" className="User_data_Nick_edit">
+                  <img src={EditPencil} alt="edit" className="User_data_Nick_edit" />
+              </button>
+            </div>
+            <span className='User_data_text'>Age: {age}</span>
+            <span className='User_data_text'>your best score: {score}</span>
 
 
 
