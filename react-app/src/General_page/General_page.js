eb153cbd1424a28b6ddeff373440b0b67f228e69
Maksym45 <maks.meleshko12@gmail.com>
2023-11-19 19:15:58 +0200

    made simple design for general page

--- react-app/src/General_page/General_page.js
+++ react-app/src/General_page/General_page.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './General_page.css';
 
 export default function Dashboard() {
   return(
     <div className='General'>
-        <h2>Dashboard</h2>
-        <Link to="/leaderboard">
-            <h2>leaderboard</h2>
-        </Link>
-        <Link to="/account">
-            <h2>User_account</h2>
-        </Link>
+         <div className='General_name'>
+           Name of the game
+         </div>
+
+        <div className='General_head'>
+            <Link to="/leaderboard">
+                <h2 className='General_head_text'>leaderboard</h2>
+            </Link>
+
+            <Link to="/account">
+                <h2 className='General_head_text'>User_account</h2>
+            </Link>
+        </div>
+        <div>
+
+        </div>
     </div>
   );
 }
