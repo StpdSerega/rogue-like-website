2f8acafebc61a8c21360866af773e42cf867bd79
Maksym45 <maks.meleshko12@gmail.com>
2023-11-30 17:05:57 +0200

    deleted_unnecessary_files

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 import "./styles.css";
-import "./Auth-components/App/Auth.css";
+//import "./Auth-components/App/Auth.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./Auth-components/Dashboard/Dashboard";
-import Preferences from "./Auth-components/Preferences/Preferences";
 import Leaderboard from "./Leaderboard/Leaderboard";
 import UserAccount from "./User_account/User_account";
 import GeneralPage from "./General_page/General_page";
@@ -17,8 +15,6 @@ function App() {
       
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/preferences" element={<Preferences />} />
           <Route path="/account" element={<UserAccount />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/global-leaderboard" element={<GlobalLeaderboard />} />
